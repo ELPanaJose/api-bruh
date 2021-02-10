7fabd836a15722778fd303991f3fdc398e447fa4
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-02-09 23:47:14 -0500

    add an incrementer

--- api-bruh/main.go
+++ api-bruh/main.go
@@ -1,21 +1,25 @@
 package main
 
-import(
+import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"encoding/json"
-	"io/ioutil"
 )
 
 type Meme struct {
 	Title string `json:"Title"`
-	Link string `json:"link"`
+	Link  string `json:"link"`
 }
 
 type Memes []Meme
 
-func allMemes(w http.ResponseWriter, r *http.Request){
+func allMemes(w http.ResponseWriter, r *http.Request) {
+	for i := 0; i <= 1; i++ {
+		incrementar()
+		fmt.Println("request number :  ", contador-1)
+	}
 	data, err := ioutil.ReadFile("./memes.json")
 	if err != nil {
 		fmt.Print(err)
@@ -27,21 +31,26 @@ func allMemes(w http.ResponseWriter, r *http.Request){
 		fmt.Print(err)
 	}
 
-	fmt.Println("all memes here")
 	json.NewEncoder(w).Encode(memes)
 }
 
-func homePage(w http.ResponseWriter, f *http.Request){
+var contador = 0
+
+func homePage(w http.ResponseWriter, f *http.Request) {
 	fmt.Fprintf(w, "just for fun, a little api with memes👌, api good, go to api-bruh.elpanajose.repl.co/memes")
 }
 
-func setUpRoutes(){
+func incrementar() {
+	contador++
+}
+
+func setUpRoutes() {
 	http.HandleFunc("/", homePage)
 	http.HandleFunc("/memes", allMemes)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func main(){
-	fmt.Println("BRUH")
+func main() {
+	fmt.Println("server on port 8080")
 	setUpRoutes()
 }
